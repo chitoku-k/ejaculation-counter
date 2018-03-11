13829b16fde4aba080d35ce60d840fba1399ebca
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-03-11 12:57:24 +0900

    実務経験ガチャn連対応

--- src/ShikoAction.js
+++ src/ShikoAction.js
@@ -274,7 +274,7 @@ class ThroughShikoAction extends ShikoAction {
 
 class MpywShikoAction extends ShikoAction {
     get regex() {
-        return /(mpyw|まっぴー|実務経験)(が|ガ|ｶﾞ)[チﾁ][ャｬ]/;
+        return /(mpyw|まっぴー|実務経験)((\d+)連)?(が|ガ|ｶﾞ)[チﾁ][ャｬ]/;
     }
 
     get api() {
@@ -286,10 +286,15 @@ class MpywShikoAction extends ShikoAction {
             return;
         }
 
+        const count = parseInt(this.regex.exec(status.text)[3] || 1);
+
         try {
             const mpyw = await request({
                 method: "HEAD",
                 uri: this.api,
+                qs: {
+                    count: count,
+                },
                 simple: false,
                 followRedirect: false,
                 resolveWithFullResponse: true,
