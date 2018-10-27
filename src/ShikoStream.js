cfe3dd41489f7e2abbd42507133590313fc2debb
Chitoku <odango@chitoku.jp>
2018-10-28 03:19:18 +0900

    Add auto-calculated reconnection in ShikoStream

--- src/ShikoStream.js
+++ src/ShikoStream.js
@@ -1,23 +1,54 @@
 const PQueue = require("p-queue");
 const WebSocket = require("ws");
-const { fromEvent, from } = require("rxjs");
+const { fromEvent, from, Observable } = require("rxjs");
 const { filter, map, mergeMap, toArray } = require("rxjs/operators");
+
+const RECONNECT_NONE = 0;
+const RECONNECT_INITIAL = 5;
+const RECONNECT_MAX = 320;
 
 exports.ShikoStream = class ShikoStream {
     constructor(service, actions) {
         this.service = service;
         this.actions = actions;
+        this.reconnect = 0;
         this.queue = new PQueue({
             concurrency: 1,
         });
     }
 
     create() {
-        this.stream = new WebSocket(
-            `${process.env.MASTODON_WSS_URL}streaming?access_token=${process.env.MASTODON_ACCESS_TOKEN}&stream=user`,
-        );
+        this.stream = Observable.create(observer => {
+            const socket = new WebSocket(
+                `${process.env.MASTODON_WSS_URL}streaming?access_token=${process.env.MASTODON_ACCESS_TOKEN}&stream=user`,
+            );
 
-        fromEvent(this.stream, "message").pipe(
+            fromEvent(socket, "open").subscribe(() => {
+                console.log("ShikoStream has established.");
+
+                // Resets retry interval value when the connection is established
+                this.reconnect = 0;
+            });
+
+            fromEvent(socket, "close").subscribe(() => {
+                // Interval value will be doubled on every retry to a maximum of RECONNECT_MAX ms
+                this.reconnect = Math.min(
+                    Math.max(
+                        this.reconnect * 2,
+                        RECONNECT_INITIAL,
+                    ),
+                    RECONNECT_MAX,
+                );
+
+                console.warn(`ShikoStream was closed. Reconnecting in ${this.reconnect} s...`);
+                setTimeout(() => this.create(), this.reconnect * 1000);
+            });
+
+            fromEvent(socket, "message").subscribe((...args) => observer.next(...args));
+            fromEvent(socket, "error").subscribe((...args) => observer.error(...args));
+        });
+
+        this.stream.pipe(
             map(x => JSON.parse(x.data)),
             filter(x => x.event === "update"),
             map(x => JSON.parse(x.payload)),
@@ -36,28 +67,8 @@ exports.ShikoStream = class ShikoStream {
             )),
             mergeMap(x => x, (outer, inner) => inner),
         ).subscribe(
-            (...args) => this.onMessage(...args),
+            ({ action, toot }) => this.queue.add(() => action.invoke(toot)),
+            error => console.error(error.message),
         );
-
-        fromEvent(this.stream, "error").subscribe(
-            (...args) => this.onError(...args),
-        );
-
-        fromEvent(this.stream, "close").subscribe(
-            (...args) => this.onClose(...args),
-        );
-    }
-
-    onMessage({ action, toot }) {
-        this.queue.add(() => action.invoke(toot));
-    }
-
-    onError(error) {
-        console.error(error);
-    }
-
-    onClose() {
-        console.warn("Connection was closed. Reconnecting...");
-        this.create();
     }
 }
