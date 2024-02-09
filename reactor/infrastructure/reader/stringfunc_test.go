59398d501788b555f9fc51d7a9bee9b8aab8b5e7
Chitoku <odango@chitoku.jp>
2024-02-10 06:48:12 +0900

    Regenerate mock files

--- reactor/infrastructure/reader/stringfunc_test.go
+++ reactor/infrastructure/reader/stringfunc_test.go
@@ -5,9 +5,9 @@ import (
 	"testing/iotest"
 
 	"github.com/chitoku-k/ejaculation-counter/reactor/infrastructure/reader"
-	"go.uber.org/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
 )
 
 var _ = Describe("StringFuncReader", func() {
