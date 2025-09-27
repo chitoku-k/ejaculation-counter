e51cf7ce44ceae446a9954b370bb2e23b39d596a
Chitoku <chitoku-k@users.noreply.github.com>
2025-09-28 02:20:33 +0900

    Add grafana-export (#992)

---
+++ grafana-export/go.mod
@@ -0,0 +1,11 @@
+module github.com/chitoku-k/ejaculation-counter/grafana-export
+
+go 1.25
+
+require github.com/playwright-community/playwright-go v0.5200.1
+
+require (
+	github.com/deckarep/golang-set/v2 v2.7.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.4 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+)
---
+++ grafana-export/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/playwright-community/playwright-go"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		println("Usage:")
+		println("  " + os.Args[0] + " URL")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+
+	if err := playwright.Install(&playwright.RunOptions{SkipInstallBrowsers: true}); err != nil {
+		log.Fatalf("Failed to install playwright: %v", err)
+	}
+
+	pw, err := playwright.Run()
+	if err != nil {
+		log.Fatalf("Failed to run playwright: %v", err)
+	}
+	defer func() {
+		_ = pw.Stop()
+	}()
+
+	browser, err := pw.Firefox.Launch()
+	if err != nil {
+		log.Fatalf("Failed to launch browser: %v", err)
+	}
+	defer func() {
+		_ = browser.Close()
+	}()
+
+	page, err := browser.NewPage()
+	if err != nil {
+		log.Fatalf("Failed to create a new page: %v", err)
+	}
+	defer func() {
+		_ = page.Close()
+	}()
+
+	response, err := page.Goto(url)
+	if err != nil {
+		log.Fatalf("Failed to navigate: %v", err)
+	}
+	if status := response.Status(); status != http.StatusOK {
+		log.Fatalf("Failed to load (HTTP %v)", status)
+	}
+
+	toggle := page.Locator(`[data-testid="data-testid export as json externally switch"]`)
+	if err := toggle.DispatchEvent("click", nil); err != nil {
+		log.Fatalf("Failed to check the toggle: %v", err)
+	}
+
+	download, err := page.ExpectDownload(func() error {
+		button := page.Locator(`[data-testid="data-testid export as json save to file button"]`)
+		return button.DispatchEvent("click", nil)
+	})
+	if err != nil {
+		log.Fatalf("Failed to download: %v", err)
+	}
+	if err := download.SaveAs("/dest/dashboards/ejaculation-counter.json"); err != nil {
+		log.Fatalf("Failed to save: %v", err)
+	}
+
+	log.Println("Exported.")
+}
